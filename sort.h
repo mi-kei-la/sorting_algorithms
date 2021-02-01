422afe761c205f6f93514c093cd5eb1941c92a2f
mi-kei-la <micaela.percovich@gmail.com>
2021-02-01 20:25:16 +0000

    update header file

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+int part(int *array, int lo, int hi, size_t size);
+void quicker(int *array, int lo, int hi, size_t size);
 void quick_sort(int *array, size_t size);
 void insert_node(listint_t *i, listint_t *j, listint_t **list);
 
