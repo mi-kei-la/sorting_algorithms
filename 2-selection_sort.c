7563c7761e9e752c54366a657d28e5c4a5c6f52b
mi-kei-la <micaela.percovich@gmail.com>
2021-02-01 04:54:58 +0000

    add new task

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ *
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, x = 0, last_pos = 0;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		last_pos = i;
+		for (j = i; j < size; j++)
+		{
+			if ((array[j] < array[i]) && (array[j] < array[last_pos]))
+				last_pos = j;
+		}
+		if (array[last_pos] != array[i])
+		{
+			x = array[i];
+			array[i] = array[last_pos];
+			array[last_pos] = x;
+			print_array(array, size);
+		}
+	}
+}
